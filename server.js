edb90595222ba16cb31bdd0834b13cf64f90158e
spettigrew <sara.pettigrew5@gmail.com>
2020-01-16 14:20:41 -0700

    Session config added to server file

--- server.js
+++ server.js
@@ -1,17 +1,35 @@
 express = require("express")
 const helmet = require("helmet")
 const cors = require("cors")
-
-const server = express()
-server.use(helmet())
-server.use(cors())
-server.use(express.json())
-
-require("dotenv").config()
+const session = require("express-session")
 
 // import routers
 const authRouter = require("./auth/auth-router")
 const userRouter = require("./users/user-router")
+
+const server = express()
+
+const sessionConfig = {
+    name: "cookie",  //default name is sid (hackers will know we're using sessions)
+    secret: "this is a secret cookie",
+    cookie: {
+        maxAge: 1000 * 30, 
+        // 1 sec * 30 sec only, then the cookie will expire
+        secure: false, //change when you go into production = true
+        httpOnly: true, //this cookie can not be accessed through JS.
+    },
+    resave: false, //do not re-create the cookie. Save it.
+    saveUninitialized: false, //GDPR laws against setting cookies automatically. Once user opts to save, change to true.
+};
+
+
+server.use(helmet())
+server.use(cors())
+server.use(express.json())
+server.use(session(sessionConfig))
+
+require("dotenv").config()
+
 
 // import api endpoints, router files
 server.use("/users", userRouter)
