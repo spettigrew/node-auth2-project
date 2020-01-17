4a31f75a013ba50cb9737e35783cbf0a74a2c9ba
spettigrew <sara.pettigrew5@gmail.com>
2020-01-16 21:52:14 -0700

    Moved next() below else statement

--- middleware/restricted.js
+++ middleware/restricted.js
@@ -1,8 +1,8 @@
 module.exports = (req, res, next) => {
     if (!req.session && !req.session.user) {
-        next()
-    } else {
         res.status(401).json({ message: "Incorrect credentials provided." })
+    } else {
+        next()
     }
 }
 // user is on a session after a successful login. Restricts access to resources using middleware with sessions.
