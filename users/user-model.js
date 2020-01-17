5d4a0e5d59bfec816cf8d00904b41affd628684d
spettigrew <sara.pettigrew5@gmail.com>
2020-01-16 21:32:02 -0700

    Db file name changed from "user" to "users"

--- users/user-model.js
+++ users/user-model.js
@@ -2,12 +2,12 @@ const bcrypt = require("bcryptjs")
 const db = require("../data/db-config")
 
 function find() {
-    return db("user")
+    return db("users")
     .select("id", "username")
 }
 
 function findBy(filter) {
-    return db("user")
+    return db("users")
     .where(filter)
     .select("id", "username", "password")
 }
@@ -15,14 +15,14 @@ function findBy(filter) {
 async function add(user) {
     user.password = await bcrypt.hash(user.password, 12)
 
-    const [id] = await db("user")
+    const [id] = await db("users")
     .insert(user)
 
     return findById(id)
 }
 
 function findById(id) {
-    return db("user")
+    return db("users")
     .where({ id })
     .first("id", "username")
 }
