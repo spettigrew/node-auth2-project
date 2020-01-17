a0cb98120da7bbec6e44ca26e1f063a590c1aee9
spettigrew <sara.pettigrew5@gmail.com>
2020-01-16 20:13:32 -0700

    Moved and renamed middleware file

--- middleware/restricted.js
+++ middleware/restricted.js
@@ -1,5 +1,5 @@
 module.exports = (req, res, next) => {
-    if (req.session && req.session.user) {
+    if (!req.session && !req.session.user) {
         next()
     } else {
         res.status(401).json({ message: "Incorrect credentials provided." })
